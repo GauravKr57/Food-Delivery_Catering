36e943b74f09380d2c1c519d677f1f7613310816
GauravKr57 <gkcric987@gmail.com>
2023-08-06 21:36:54 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+// var length= document.querySelectorAll('#navbar ul li a').length;
+
+// for(var i=0;i<length;i++){
+
+// var navClick= document.querySelectorAll('#navbar ul li a')[i].addEventListener("click",function(){
+//     this.style.color="rgb(245, 74, 48)"
+// })
+
+// }
